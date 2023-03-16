438874581bad7f2c82db6a5aa7abd647d602afc7
Bigtoxa766 <bigtoxa766@gmail.com>
2023-03-17 01:49:40 +0200

    update task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
 
+const startBtnEl = document.querySelector('button[data-start]');
+const stopBtnEl = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body')
+
+let timerId = null;
+
+startBtnEl.addEventListener('click', onStartBtn)
+
+function onStartBtn() {
+  
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor()
+  }, 1000)
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+stopBtnEl.addEventListener('click', () => {
+  clearInterval(timerId)
+})
